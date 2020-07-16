25955da096d90bc22b1389a4682c6fc7e6549d3f
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-16 13:43:15 +0200

    fgfgfg

---
+++ WatchOut/Assets/Scenes/Macchinari/DragAndDrop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler,IEndDragHandler
+{
+
+    private float x;
+    private float y;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+        x = transform.position.x;
+        y = transform.position.y;
+    }
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Vector3 temp = new Vector3(x,y,0);
+        transform.position=temp;
+    }
+    
+}
