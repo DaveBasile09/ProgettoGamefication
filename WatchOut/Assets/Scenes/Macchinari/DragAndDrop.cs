b47df27b99d0737430c1290daa5f6eafbeeb523f
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-18 15:56:08 +0200

    cambiato lo script e reso singolo

--- WatchOut/Assets/Scenes/Macchinari/DragAndDrop.cs
+++ WatchOut/Assets/Scenes/Macchinari/DragAndDrop.cs
@@ -9,57 +9,19 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
     
     private float x=0;
     private float y=0;
-    private string nome;
-    public GameObject M500F;
-    public GameObject M100F;
-    public GameObject M500;
-    public GameObject M100;
+    public GameObject Mezza;
     public GameObject Testo;
     
     
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (gameObject.name == "500" && other.transform.name=="FessuraBanconote")
+        if (other.transform.name == "FessuraBanconote")
         {
-           
-            Testo.SetActive(true);
-            StartCoroutine(wait());
-            gameObject.SetActive(false);
-            M500.SetActive(true);
-            M500F.SetActive(false);
-            M100F.SetActive(false);
-            M100.SetActive(false);
-            
-        }
-        if (gameObject.name == "500F" && other.transform.name=="FessuraBanconote")
-        {
-            Testo.SetActive(true);
-            StartCoroutine(wait());
-            gameObject.SetActive(false);
-            M500F.SetActive(true);
-            M100F.SetActive(false);
-            M500.SetActive(false);
-            M100.SetActive(false);
-        } 
-        if (gameObject.name == "100" && other.transform.name=="FessuraBanconote")
-        {
-            Testo.SetActive(true);
-            StartCoroutine(wait());
-            gameObject.SetActive(false);
-            M100.SetActive(true);
-            M500F.SetActive(false);
-            M100F.SetActive(false);
-            M500.SetActive(false);
-        } 
-        if (gameObject.name == "100F" && other.transform.name=="FessuraBanconote")
-        {
-            Testo.SetActive(true);
-            StartCoroutine(wait());
-            gameObject.SetActive(false);
-            M100F.SetActive(true);
-            M500F.SetActive(false);
-            M500.SetActive(false);
-            M100.SetActive(false);
+            if(!Testo.activeInHierarchy)
+                Testo.SetActive(true);
+                StopCoroutine(wait());
+                gameObject.SetActive(false);
+                Mezza.SetActive(true);
         }
     }
 
@@ -75,11 +37,7 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
         Debug.Log("inizio");
         x = transform.position.x;
         y = transform.position.y;
-        nome = transform.name;
-        M500F.SetActive(false);
-        M100F.SetActive(false);
-        M500.SetActive(false);
-        M100.SetActive(false);
+        Mezza.SetActive(false);
         Testo.SetActive(false);
         StartCoroutine(wait());
     }
