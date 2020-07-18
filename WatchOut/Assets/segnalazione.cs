1c2d45a7a326ba897292ed659e8c32e3c29a0f88
rafmc98 <65073971+rafmc98@users.noreply.github.com>
2020-07-18 14:18:41 +0200

    sistemazioni random

---
+++ WatchOut/Assets/segnalazione.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class segnalazione : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Script attivo sull'oggetto " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
