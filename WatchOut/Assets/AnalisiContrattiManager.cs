3268954b0d715e7a633c9a6a363d8eefa47ddae1
DaveBasile09 <64369787+DaveBasile09@users.noreply.github.com>
2020-07-19 17:05:56 +0200

    tuttoeniente

---
+++ WatchOut/Assets/AnalisiContrattiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnalisiContrattiManager : MonoBehaviour
+{
+    public static bool cl1=false;
+    public static bool cl2=false;
+    public static bool cl3=false;
+
+    public void indietro()
+    {
+       
+        ChiediOperatoreManager.indietro();
+    }
+    public void termina()
+    {
+        ChiediOperatoreManager.c1 = true;
+        ChiediOperatoreManager.termina();
+    }
+    public void clausola1()
+    {
+        if (!cl1)
+        {
+            SceneManager.LoadScene("SegnalazionePresenzaClausola1", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ WatchOut/Assets/ChiediOperatoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChiediOperatoreManager : MonoBehaviour
+
+{   
+    public static bool c1 =false;
+    public static bool c2=false;
+    public static bool c3=false;
+public void avvia1()
+    {
+        if (!c1)
+        {
+            SceneManager.LoadScene("Analisi_Contratti", LoadSceneMode.Single);
+        }
+    }
+    public static void indietro()
+    {
+        SceneManager.LoadScene("Chiedi_Operatore", LoadSceneMode.Single);
+    }
+    public static void termina()
+    {
+        SceneManager.LoadScene("Chiedi_Operatore", LoadSceneMode.Single);
+    }
+}
