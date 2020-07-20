2ced19198e1de81241fdbebee246eab64de675c1
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-20 13:56:51 +0200

    mi fermo con il drag and drop di idoneità perchè tocca chiacchierare con bruni

--- WatchOut/Assets/Scenes/Macchinari/DragAndDropIdoneità.cs
+++ WatchOut/Assets/Scenes/Macchinari/DragAndDropIdoneità.cs
@@ -7,11 +7,34 @@ public class DragAndDropIdoneità : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     private float x=0;
     private float y=0;
+    public GameObject Fessura;
+    public GameObject mezza1;
+    public GameObject mezza2;
+    private static GameObject ultimo1;
+    private static GameObject ultimo2;
+
     
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.gameObject == Fessura)
+        {
+            if(ultimo1!=null && ultimo2!=null)
+                ultimo1.SetActive(false);
+                ultimo2.SetActive(false);
+            gameObject.SetActive(false);
+            mezza1.SetActive(true);
+            mezza2.SetActive(true);
+            ultimo1 = mezza1;
+            ultimo2 = mezza2;
+        }
+    }
+
     void Start()
     {
         x = transform.position.x;
         y = transform.position.y;
+        mezza1.SetActive(false);
+        mezza2.SetActive(false);
     }
 
    
