d02ddff6765f7d2bcf64a706eec25e34a294d92e
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-20 17:14:52 +0200

    finito drag and drop test Idoneita

--- WatchOut/Assets/Scenes/Macchinari/DeDIdoneità.cs
+++ WatchOut/Assets/Scenes/Macchinari/DeDIdoneità.cs
@@ -1,31 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System;
 
-public class DragAndDropIdoneità : MonoBehaviour, IDragHandler, IEndDragHandler
+
+public class DeDIdoneità : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     private float x = 0;
     private float y = 0;
     public GameObject mezza1;
     public GameObject mezza2;
     public GameObject Fessura;
+    public GameObject percLogore;
+    public GameObject percBuone;
     private static GameObject ultimo1;
     private static GameObject ultimo2;
 
-    
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.gameObject == Fessura)
         {
-            if(ultimo1!=null && ultimo2!=null)
+            if (ultimo1 != null)
+            {
                 ultimo1.SetActive(false);
-            ultimo2.SetActive(false);
-            gameObject.SetActive(false);
+                ultimo2.SetActive(false);
+            }
             mezza1.SetActive(true);
             mezza2.SetActive(true);
+            percBuone.SetActive(true);
+            percLogore.SetActive(true);
             ultimo1 = mezza1;
             ultimo2 = mezza2;
+            gameObject.SetActive(false);
+
         }
     }
 
@@ -35,6 +45,9 @@ public class DragAndDropIdoneità : MonoBehaviour, IDragHandler, IEndDragHandler
         y = transform.position.y;
         mezza1.SetActive(false);
         mezza2.SetActive(false);
+        percBuone.SetActive(false);
+        percLogore.SetActive(false);
+       
     }
 
    
