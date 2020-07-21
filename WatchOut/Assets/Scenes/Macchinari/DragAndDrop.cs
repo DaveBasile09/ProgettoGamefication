0e1f3a2a85e70d5e1fc42bad096930efea8dd0e1
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-21 18:03:28 +0200

    Sistemato autenticità e bottoni segnlazione

--- WatchOut/Assets/Scenes/Macchinari/DragAndDrop.cs
+++ WatchOut/Assets/Scenes/Macchinari/DragAndDrop.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
 {
@@ -14,7 +17,10 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
     public GameObject Testo;
     public GameObject Fessura;
     private static GameObject ultimo;
-
+    public static bool v1=true;
+    public static bool v2=true;
+    public static bool v3=true;
+    public static bool v4=true;
    
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,11 +31,31 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
             GameObject obj=Instantiate(Testo,transform.parent);
             Destroy(obj,5);
             gameObject.SetActive(false);
+            Disa(gameObject);
             Mezza.SetActive(true);
             ultimo = Mezza;
+            
+            
         }
     }
 
+    void Disa(GameObject b)
+    {
+        switch (b.transform.name)
+        {
+            case "500": v1 = false;
+                        break;
+            case "100": v2 = false;
+                        break;
+            case "500F": v3 = false;
+                        break;
+            case "100F": v4 = false;
+                        break;
+            
+        }
+        
+        
+    }
    
     void Start()
     {
@@ -48,8 +74,8 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
     {
         transform.position=new Vector3(x,y,0);
     }
-    
-    
-    
+
+   
     
 }
+
