30208d4f74027826b292014c2320fc36f28fe165
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-07-23 18:37:23 +0200

    Fine Autenticità e creazione scenario di scelta

--- WatchOut/Assets/Scenes/Macchinari/Autenticità/SegnalaAut.cs
+++ WatchOut/Assets/Scenes/Macchinari/Autenticità/SegnalaAut.cs
@@ -14,6 +14,11 @@ public class SegnalaAut : MonoBehaviour
     
     void Start()
     {
+        Text t1 = (Text)GameObject.Find("soldi").GetComponent("Text");
+        Text t2 = (Text)GameObject.Find("reputazione").GetComponent("Text");
+        t1.text = System.Convert.ToString(HomeManager.soldi);
+        t2.text = System.Convert.ToString(HomeManager.reputazione);
+        
         GameObject.Find("500").SetActive(DragAndDrop.v1);
         GameObject.Find("100").SetActive(DragAndDrop.v2);
         GameObject.Find("500F").SetActive(DragAndDrop.v3);
@@ -33,6 +38,12 @@ public class SegnalaAut : MonoBehaviour
             case "pulsante3":   toggleX("X3");  
                                 break;
             case "pulsante4":   toggleX("X4");  
+                                break;
+            case "pulsante5":   toggleX("X5");  
+                                break;
+            case "pulsante6":   toggleX("X6");  
+                                break;
+            case "pulsante7":   toggleX("X7");  
                                 break;
         }
     }
---
+++ WatchOut/Assets/Scenes/Macchinari/Scelta/cambiascena.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class cambiascena : MonoBehaviour
+{
+
+    public static bool Ido=false;
+    public static bool aut=false;
+    void Start()
+    {
+        Text t1 = (Text)GameObject.Find("soldi").GetComponent("Text");
+        Text t2 = (Text)GameObject.Find("reputazione").GetComponent("Text");
+        t1.text = System.Convert.ToString(HomeManager.soldi);
+        t2.text = System.Convert.ToString(HomeManager.reputazione);
+        if (HomeManager.soldi <= 0 || HomeManager.reputazione<=0)
+        {
+            //vai al resoconto
+        }
+    }
+
+    public void avviaIdo()
+    {
+        if (!Ido)
+        {
+            SceneManager.LoadScene("TestIdoneità", LoadSceneMode.Single);
+        }
+    }
+
+    public void avviaAut()
+    {
+        if (!aut)
+        {
+            SceneManager.LoadScene("TestAutenticità", LoadSceneMode.Single);
+        }
+    }
+
+    public void home()
+    {
+        SceneManager.LoadScene("Scenes/HomeBanca/HomeBanca", LoadSceneMode.Single);
+
+    }
+
+   
+}
